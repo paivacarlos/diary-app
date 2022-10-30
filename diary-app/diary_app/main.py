b72e5f16f4b9ff9cb91aa32852f5de86185ebc09
Carlos Paiva <paivakk22@gmail.com>
2022-10-30 10:07:30 -0300

    Create the endpoint and the class to response temporarily

--- diary-app/diary_app/main.py
+++ diary-app/diary_app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
+from resource_app.post_resource import Create_Fact_Diary
+
 app = FastAPI()
 
 @app.get("/health")
 async def root():
     return {"message": "We are online! :)"}
+
+@app.post("/create-fact")
+async def create_fact(new_fact: Create_Fact_Diary):
+    response_body = {
+        "message": "Fact Created with success!",
+        "fact": new_fact
+    }
+    return response_body
+
