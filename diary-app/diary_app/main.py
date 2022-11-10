d00235ce9ef01fd82800387547b145e90a567725
Carlos Paiva <paivakk22@gmail.com>
2022-11-10 08:49:08 -0300

    Created a provisional database

--- diary-app/diary_app/main.py
+++ diary-app/diary_app/main.py
@@ -4,11 +4,14 @@ from resource_app.post_resource import Create_Fact_Diary
 
 app = FastAPI()
 
+database = []
+
 @app.get("/health")
 async def root():
     return {"message": "We are online! :)"}
 
 @app.post("/create-fact")
 async def create_fact(new_fact: Create_Fact_Diary):
+    database.append(new_fact)
     response_body = new_fact
     return response_body
