8f4c1fa56e74c61611410c5d1d3653d242f09fb2
Carlos Paiva <paivakk22@gmail.com>
2023-02-25 14:42:55 -0300

    argument of request solved

--- diary-app/app/controllers/update_fact_controller.py
+++ diary-app/app/controllers/update_fact_controller.py
@@ -1,16 +1,12 @@
-import json
-
 from fastapi import APIRouter, HTTPException
 from database.database import database
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
 from models.create_fact_models import Create_Fact
 
 router = APIRouter()
 
 
 @router.put("/")
-async def update_fact(db=database, id_fact=str, data_update_fact=Create_Fact):
+async def update_fact(data_update_fact: Create_Fact, db=database, id_fact=str):
     response_body = None
     controller = False
 
@@ -24,7 +20,7 @@ async def update_fact(db=database, id_fact=str, data_update_fact=Create_Fact):
                 db.pop(fact)
                 db.append({
                     'id': id_fact_from_request,
-                    'data': 'teste'
+                    'data': data_update_fact
                 })
                 response_body = {'updated fact:': db[fact]}
                 controller = True
